d7aadbdf72ebc402812d0a9f77811e4315baf30c
Baptiste Lefort <71429321+blefo@users.noreply.github.com>
2024-05-15 09:32:14 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'fastinference-llm',         # How you named your package folder (MyLib)
   packages = ['fastinference'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Seamlessly integrate with top LLM APIs for speedy, robust, and scalable querying. Ideal for developers needing quick, reliable AI-powered responses.',   # Give a short description about your library
   author = 'Baptiste Lefort',                   # Type in your name
